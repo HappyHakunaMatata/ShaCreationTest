43ead7c7817af4cb51ddc58c5a4bacf184e8b700
HappyHakunaMatata <122421848+HappyHakunaMatata@users.noreply.github.com>
2024-01-02 20:24:09 +0100

    dev-0.0.1

---
+++ ShaCreationTest/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace ShaCreationTest
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var i = 0;
+            double time = 0;
+            while (i < 10)
+            {
+                var timer = Stopwatch.StartNew();
+                Hash();
+                timer.Stop();
+                time += timer.Elapsed.TotalMilliseconds;
+                i += 1;
+            }
+            Console.WriteLine($"Avg Default Timer: {time / 10:F3} ms");
+            i = 0;
+            time = 0;
+            while (i < 10)
+            {
+                var timer = Stopwatch.StartNew();
+                HashLoop(10000);
+                timer.Stop();
+                time += timer.Elapsed.TotalMilliseconds;
+                i += 1;
+            }
+            Console.WriteLine($"Avg Loop Timer: {time / 10:F3} ms");
+            i = 0;
+            time = 0;
+            while (i < 10)
+            {
+                var timer = Stopwatch.StartNew();
+                HashParallel();
+                timer.Stop();
+                time += timer.Elapsed.TotalMilliseconds;
+                i += 1;
+            }
+            Console.WriteLine($"Avg Loop Timer: {time / 10:F3} ms");
+        }
+
+        public static void Hash()
+        {
+            using (SHA256 sHA256 = SHA256.Create())
+            {
+                BigInteger max = new BigInteger(1) << 128;
+                RandomBigInteger random = new RandomBigInteger();
+                var value = random.NextBigInteger(0, max);
+                var data = sHA256.ComputeHash(value.ToByteArray());
+            }
+        }
+
+        public static void HashLoop(int limit)
+        {
+            var i = 0;
+            while (i < limit)
+            {
+                using (SHA256 sHA256 = SHA256.Create())
+                {
+                    BigInteger max = new BigInteger(1) << 128;
+                    RandomBigInteger random = new RandomBigInteger();
+                    var value = random.NextBigInteger(0, max);
+                    var data = sHA256.ComputeHash(value.ToByteArray());
+                }
+                i += 1;
+            }
+        }
+
+        public static void HashParallel()
+        {
+            Parallel.For(0, 100000, val =>
+            {
+                using (SHA256 sHA256 = SHA256.Create())
+                {
+                    BigInteger max = new BigInteger(1) << 128;
+                    RandomBigInteger random = new RandomBigInteger();
+                    var value = random.NextBigInteger(0, max);
+                    var data = sHA256.ComputeHash(value.ToByteArray());
+                }
+            });
+        }
+    }
+}
---
+++ ShaCreationTest/RandomBigInteger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+namespace ShaCreationTest
+{
+	public class RandomBigInteger:Random
+	{
+		public RandomBigInteger():base()
+		{
+		}
+
+        public RandomBigInteger(int Seed) : base(Seed)
+        {
+
+        }
+
+		public BigInteger NextBigInteger(int bitLenght)
+		{
+			if (bitLenght < 1)
+			{
+				return BigInteger.Zero;
+			}
+			int bytes = bitLenght / 8;
+			int bits = bitLenght % 8;
+			byte[] bs = new byte[bytes + 1];
+			NextBytes(bs);
+			byte mask = (byte)(0xFF >> (8 - bits));
+			bs[bs.Length - 1] &= mask;
+			return new BigInteger(bs);
+		}
+
+		public BigInteger NextBigInteger(BigInteger start, BigInteger end)
+		{
+			if (start == end)
+			{
+				return start;
+
+			}
+			BigInteger res = end;
+			if (start > end)
+			{
+				end = start;
+				start = res;
+				res = end - start;
+
+			}
+			else
+			{
+				res -= start;
+			}
+
+			byte[] bs = res.ToByteArray();
+			int bits = 8;
+			byte mask = 0x7F;
+			while ((bs[bs.Length-1] & mask) == bs[bs.Length - 1])
+			{
+				bits--;
+				mask >>= 1;
+
+			}
+			bits += 8 * bs.Length;
+			return ((NextBigInteger(bits + 1) * res) / BigInteger.Pow(2, bits + 1)) + start;
+		}
+
+
+    }
+}
+
